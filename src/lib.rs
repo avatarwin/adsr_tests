bc63da181e1432fceb3eeebed0fdda4b3df53997
AvatarWin <133696+avatarwin@users.noreply.github.com>
2018-04-13 21:23:37 +0100

    Tidying

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 #![allow(unused_variables)]
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
-#[macro_use]
 
+#[macro_use]
 extern crate assert_approx_eq;
 
 pub mod adsr {
   pub struct Envelope {
-    triggered: Option<f64>,
+    triggered: Option< f64 >,
     attack: f64,
     decay: f64,
     sustain: f64,
@@ -24,21 +24,25 @@ pub mod adsr {
                         release: 0.0, };
     }
 
-    pub fn trigger(&mut self, time: f64) {
+    pub fn trigger( &mut self, time: f64 ) {
       if self.triggered == None {
-        self.triggered = Some(time);
+        self.triggered = Some( time );
       } else {
         // should really have a 'retriggerable' option somewhere...
-        self.triggered = Some(time);
+        self.triggered = Some( time );
       }
     }
 
-    pub fn set_attack(&mut self, attack: f64) {
+    pub fn set_attack( &mut self, attack: f64 ) {
       self.attack = attack;
     }
 
-    pub fn get_value(&self, time: f64) -> f64 {
+    pub fn get_value( &self, time: f64 ) -> f64 {
       0.0
+    }
+
+    pub fn is_running( &self ) -> bool {
+      ( self.triggered != None )
     }
   }
 }
@@ -52,6 +56,7 @@ mod tests {
   fn it_works() {
     let env1 = Envelope::new();
 
-    assert_approx_eq!(env1.get_value(0.0), 0.0);
+    assert_approx_eq!( env1.get_value( 0.0 ), 0.0 );
+    assert!( !env1.is_running() );
   }
 }
