96fe7a6e1b317df4abd8087636f229f0f15afcc8
AvatarWin <133696+avatarwin@users.noreply.github.com>
2018-04-13 20:35:17 +0100

    Use approx equal crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,57 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(non_snake_case)]
+#![allow(unused_imports)]
+#[macro_use]
 
-mod ADSR {
-pub struct Envelope {
-  triggered: Option<f64>,
-  attack: f64,
-  decay: f64,
-  sustain: f64,
-  release: f64,
-}
+extern crate assert_approx_eq;
 
-impl Envelope {
-  pub fn trigger(&mut self, time: f64) {
-    if self.triggered == None {
-      self.triggered = Some(time);
-    } else {
-      // should really have a 'retriggerable' option somewhere...
-      self.triggered = Some(time);
+pub mod adsr {
+  pub struct Envelope {
+    triggered: Option<f64>,
+    attack: f64,
+    decay: f64,
+    sustain: f64,
+    release: f64,
+  }
+
+  impl Envelope {
+    pub fn new() -> Envelope {
+      return Envelope { triggered: None,
+                        attack: 0.0,
+                        decay: 0.0,
+                        sustain: 1.0,
+                        release: 0.0, };
+    }
+
+    pub fn trigger(&mut self, time: f64) {
+      if self.triggered == None {
+        self.triggered = Some(time);
+      } else {
+        // should really have a 'retriggerable' option somewhere...
+        self.triggered = Some(time);
+      }
+    }
+
+    pub fn set_attack(&mut self, attack: f64) {
+      self.attack = attack;
+    }
+
+    pub fn get_value(&self, time: f64) -> f64 {
+      0.0
     }
   }
-
-  pub fn set_attack( &mut self, attack: f64) {
-    self.attack = attack;
-  }
-
-  pub fn get_value( &self, time: f64 ) -> f64 {
-    0.0
-  }
 }
 
-impl Default for Envelope {
-  fn default() -> Envelope {
-    Envelope {
-      triggered: None,
-      attack: 0.0,
-      decay: 0.0,
-      sustain: 1.0,
-      release: 0.0,
-    }
-  }
-}
-}
 #[cfg(test)]
-
-use ADSR::Envelope;
-
 mod tests {
+
+  use adsr::Envelope;
+
   #[test]
   fn it_works() {
+    let env1 = Envelope::new();
 
-    let env1 = ADSR::Envelope{};
-
-    assert!(env1.get_value( 0.0 ).approx_eq(0.0));
+    assert_approx_eq!(env1.get_value(0.0), 0.0);
   }
 }
